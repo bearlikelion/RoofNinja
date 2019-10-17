1a4011147c63c561e62d590e881caf3da27ac60c
Mark Arneman <mark@arneman.me>
2019-10-17 16:58:37 -0400

    Screenshots marker text fix and sounds

---
+++ Assets/Scripts/MarkerText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerText : MonoBehaviour
+{
+    private GameObject player;
+    private Renderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!renderer.isVisible) {
+            transform.position = new Vector3(transform.position.x, player.transform.position.y + 4, transform.position.z);
+        }        
+    }
+}
